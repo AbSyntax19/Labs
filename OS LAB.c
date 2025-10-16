2aba0ffde17175878d65010dde6403f7c0c3354e
Abdur rahman <abdurrahmanwork3008@gmail.com>
2025-10-16 23:15:56 +0530

    Add process scheduling functions and main logic

--- OS LAB.c
+++ OS LAB.c
@@ -86,3 +86,63 @@ int main() {
 }
 
 '''
+
+LAB 2 B
+
+'''
+
+#include <stdio.h>
+
+void findWaitingTime(int processes[], int n, int burst_time[], int waiting_time[]) {
+    waiting_time[0] = 0;
+    for (int i = 1; i < n; i++) {
+        waiting_time[i] = 0;
+        for (int j = 0; j < i; j++) {
+            waiting_time[i] += burst_time[j];
+        }
+    }
+}
+
+void findTurnaroundTime(int processes[], int n, int burst_time[], int
+    waiting_time[], int turnaround_time[]) {
+        for (int i = 0; i < n; i++) {
+            turnaround_time[i] = burst_time[i] + waiting_time[i];
+        }
+    }
+    
+void findAverageTimes(int processes[], int n, int burst_time[]) {
+    int waiting_time[n], turnaround_time[n];
+    findWaitingTime(processes, n, burst_time, waiting_time);
+    findTurnaroundTime(processes, n, burst_time, waiting_time,
+    turnaround_time);
+    float total_waiting_time = 0, total_turnaround_time = 0;
+    for (int i = 0; i < n; i++) {
+        total_waiting_time += waiting_time[i];
+        total_turnaround_time += turnaround_time[i];
+    }
+    float avg_waiting_time = total_waiting_time / n;
+    float avg_turnaround_time = total_turnaround_time / n;
+    printf("Process\tBurst Time\tWaiting Time\tTurnaround Time\n");
+    for (int i = 0; i < n; i++) {
+        printf("P%d\t%d\t\t%d\t\t%d\n", processes[i], burst_time[i],
+        waiting_time[i], turnaround_time[i]);
+    }
+    printf("\nAverage Waiting Time: %.2f\n", avg_waiting_time);
+    printf("Average Turnaround Time: %.2f\n", avg_turnaround_time);
+}
+
+int main() {
+    int n;
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+    int processes[n], burst_time[n];
+    for (int i = 0; i < n; i++) {
+        processes[i] = i + 1;
+        printf("Enter burst time for P%d: ", i + 1);
+        scanf("%d", &burst_time[i]);
+    }
+    findAverageTimes(processes, n, burst_time);
+    return 0;
+}
+
+'''
